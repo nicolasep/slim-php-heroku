5f7e73118e47138208fb9088b396d50ba39501d9
Nicolas <nicolaseduardoperez@gmail.com>
2021-03-23 19:45:11 -0300

    practica

--- app/index.php
+++ app/index.php
@@ -1,20 +1,4 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+echo "Prueba heroku";
 
-require_once '../vendor/autoload.php';
-
-
-
-$app = new \Slim\App([]);
-
-
-
-$app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
-    return $response;
-
-});
-
-
-$app->run();
+?>
