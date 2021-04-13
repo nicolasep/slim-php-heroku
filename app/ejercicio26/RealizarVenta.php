ffd21d39efeb4a26ba779f011bcff8d2c1d0151c
Nicolas <nicolaseduardoperez@gmail.com>
2021-04-13 18:23:40 -0300

    ejercicio 26

--- app/ejercicio26/RealizarVenta.php
+++ app/ejercicio26/RealizarVenta.php
@@ -4,6 +4,7 @@ include "producto.php";
 
 include_once "ventas.php";
 
+//Nicolas Eduardo Perez
 /*Aplicación Nº 26 (RealizarVenta) Archivo: RealizarVenta.php método:POST Recibe los datos del producto(código de barra), del usuario (el id )y la cantidad de ítems ,por POST . Verificar que el usuario y el producto exista y tenga stock. crea un ID autoincremental(emulado, puede ser un random de 1 a 10.000). carga los datos necesarios para guardar la venta en un nuevo  renglón.
 Retorna un : “venta realizada”Se hizo una venta “no se pudo hacer“si no se pudo hacer Hacer los métodos necesarios en las clases
 */
